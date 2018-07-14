fd52ac35e72733047f8846de3ba3b2674b3d788c
CFoxxo <c.foxxo.m@gmail.com>
2018-07-14 19:49:00 -0400

    release

---
+++ generate.bat
@@ -0,0 +1,3 @@
+@echo off
+python usernames.py
+pause
---
+++ usernames.py
@@ -0,0 +1,11 @@
+import random
+
+def randomUsername():
+	lines = open('adjectives.txt').read().splitlines()
+	adj = random.choice(lines)
+	lines = open('nouns.txt').read().splitlines()
+	noun = random.choice(lines).capitalize()
+	return adj + noun + str(random.randint(10,99))
+
+for i in range(69):
+	print(randomUsername())
